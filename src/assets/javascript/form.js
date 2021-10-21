7915fe140fd4c1ea90b5db7f179cef414a9db199
Alexander Chi <arturochi2@hotmail.com>
2021-10-21 09:59:09 -0500

    Added change form.js set emtyp value

--- src/assets/javascript/form.js
+++ src/assets/javascript/form.js
@@ -33,12 +33,13 @@ $(document).ready( () => {
         e.preventDefault();    
         const { userName, password, isAdminLogin, totalSiteLogins } = data;
         console.log(data)
-        if( userName !== undefined || password !== undefined ) {
-            if( userName.trim() !== '' || password.trim() !== '' ) {
+        if( userName !== undefined && password !== undefined ) {
+            if( userName.trim() !== '' && password.trim() !== '' ) {
                 formData({value: totalSiteLogins + 1, name: 'totalSiteLogins'});
                 formData({value: !isAdminLogin, name: 'isAdminLogin'})
                 setTitle(!isAdminLogin);
                 $('.info').text((totalSiteLogins + 1).toFixed(2));
+                $('.error').text('')
             }else{
                 $('.error').text("User Name or Password emtyp")
             }
